69f11b6baf681dcbf583e168c5fb70df05289103
Petya Sokolov <125459815+NTPV@users.noreply.github.com>
2026-05-02 23:31:42 +0300

    Лабы 0-1
    Список продуктов

--- app/src/main/java/com/example/helloapp/MainActivity.kt
+++ app/src/main/java/com/example/helloapp/MainActivity.kt
@@ -4,12 +4,16 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Card
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.helloapp.ui.theme.HelloAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,9 +21,19 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             HelloAppTheme {
-                // A surface container using the 'background' color from the theme
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Greeting("09.03.01 ИВТ")
+                    ProductList(
+                        products = listOf(
+                            "Хлеб",
+                            "Молоко",
+                            "Яйца",
+                            "Сыр",
+                            "Яблоки",
+                            "Картофель",
+                            "Мясо",
+                            "Масло"
+                        )
+                    )
                 }
             }
         }
@@ -27,17 +41,19 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-            text = "$name \n Соколов Пётр \n Лабораторная работа !",
-            modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    HelloAppTheme {
-        Greeting("Android")
+fun ProductList(products: List<String>) {
+    LazyColumn(modifier = Modifier.padding(16.dp)) {
+        items(products) { product ->
+            Card(
+                modifier = Modifier
+                    .padding(vertical = 6.dp)
+            ) {
+                Text(
+                    text = product,
+                    modifier = Modifier.padding(12.dp),
+                    style = MaterialTheme.typography.bodyLarge
+                )
+            }
+        }
     }
 }
